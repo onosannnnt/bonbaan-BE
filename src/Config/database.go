e74742a04879bf9304b610afc449060561042ed5
Checkmartyr <p.tranf@gmail.com>
2025-01-27 04:52:07 +0700

    uodate methode to load env files
    uodate methode to load env files

--- src/Config/database.go
+++ src/Config/database.go
@@ -3,6 +3,8 @@ package Config
 import (
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/joho/godotenv"
 )
@@ -16,10 +18,30 @@ var (
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
+    _, filename, _, _ := runtime.Caller(0)
+    currentFileDir := filepath.Dir(filename)
+
+    // Walk up the directory tree to find the .env file
+    var configPath string
+    for {
+        configPath = filepath.Join(currentFileDir, ".env.test")
+        if _, err := os.Stat(configPath); !os.IsNotExist(err) {
+            break
+        }
+        parentDir := filepath.Dir(currentFileDir)
+        if parentDir == currentFileDir {
+            log.Fatalf(".env file not found")
+            os.Exit(-1)
+        }
+        currentFileDir = parentDir
+    }
+
+    // Load the .env file
+    err := godotenv.Load(configPath)
+    if err != nil {
+        log.Fatalf("Problem loading .env file: %v", err)
+        os.Exit(-1)
+    }
 
 	DbHost = os.Getenv("DB_HOST")
 	DbPort = os.Getenv("DB_PORT")
--- src/routers/user_test.go
+++ src/routers/user_test.go
@@ -8,6 +8,19 @@ import (
 	"gorm.io/gorm"
 )
 
+// type User struct {
+// 	gorm.Model
+// 	ID        uuid.UUID `gorm:"type:uuid;primaryKey;default:(uuid_generate_v4())"`
+// 	Username  string    `json:"username" gorm:"unique"`
+// 	Password  string    `json:"password" gorm:"not null"`
+// 	Email     string    `json:"email" gorm:"unique"`
+// 	Firstname string    `json:"firstname"`
+// 	Lastname  string    `json:"lastname"`
+// 	Phone     string    `json:"phone"`
+
+// }
+
+
 func TestInitUserRouter(t *testing.T) {
 	// Setup
 	app := fiber.New()
