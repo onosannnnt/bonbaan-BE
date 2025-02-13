2f3c52ff753bb75f778bbf9f681e291088f6e47f
plengguin <111556507+guayJheng@users.noreply.github.com>
2025-02-13 12:32:30 +0700

    feat : CRUD with routing

--- src/adepters/review/driver.go
+++ src/adepters/review/driver.go
@@ -22,3 +22,32 @@ func (d *ReviewDriver) Insert(review *Entities.Review) error {
 	}
 	return nil
 }
+func (d *ReviewDriver) GetAll()([]*Entities.Review,error){
+	var reviews []*Entities.Review
+	if err := d.db.Find(&reviews).Error;err != nil{
+		return nil,err
+	}
+	return reviews, nil
+}
+
+func (d *ReviewDriver) GetByID(id *string)(*Entities.Review,error){
+	var review Entities.Review
+	if err := d.db.Where("id = ?", id).First(&review).Error; err != nil {
+		return nil, err
+	}
+	return &review, nil
+
+}
+func (d *ReviewDriver) Update(id *string,review *Entities.Review) error{
+	if err := d.db.Model(review).Updates(review).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *ReviewDriver) Delete(id *string) error{
+	if err := d.db.Where("id = ?", *id).Delete(&Entities.Review{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
--- src/usecases/review/usecase.go
+++ src/usecases/review/usecase.go
@@ -6,6 +6,10 @@ import (
 
 type ReviewUsecase interface {
 	Insert(review *Entities.Review) error
+	GetAll()([]*Entities.Review,error)
+	GetByID(id *string)(*Entities.Review, error)
+	Update(id *string, review *Entities.Review) error
+	Delete(id *string) error
 }
 
 type ReviewService struct {
@@ -20,4 +24,18 @@ func NewReviewService(repo ReviewRepository) ReviewUsecase{
 
 func (s *ReviewService) Insert(review *Entities.Review) error{
 	return s.reviewRepo.Insert(review)
+	}
+
+func (s *ReviewService) GetAll()([]*Entities.Review,error){
+	return s.reviewRepo.GetAll()
+}
+func (s *ReviewService) GetByID(id *string)(*Entities.Review, error){
+	return s.reviewRepo.GetByID(id)
+}
+func (s *ReviewService) Update(id *string,review *Entities.Review) error{
+	return s.reviewRepo.Update(id, review)
+}
+
+func (s *ReviewService) Delete(id *string) error{
+	return s.reviewRepo.Delete(id)
 	}
