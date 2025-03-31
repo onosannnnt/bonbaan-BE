95bea18a3a59046e95bbc845805f41bac6f7cc6d
Santijit kumnuk <onosannnnt@Santijits-MacBook-Air.local>
2025-03-31 19:24:42 +0700

    fix : response json return error message

--- src/utils/responseJson.go
+++ src/utils/responseJson.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 func ResponseJSON(c *fiber.Ctx, statusCode int, message string, err error, data any) error {
 	return c.Status(statusCode).JSON(fiber.Map{
 		"message": message,
-		"error":   err,
+		"error":   err.Error(),
 		"data":    data,
 	})
 }
